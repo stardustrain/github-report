a1b33abc56bbce80b922880ca6a2d87a3c043f59
Lucas Han <kthanterran@gmail.com>
2020-01-27 22:15:16 +0900

    resolve #1 add report mixins. (#4)
    * resolve #1 add report mixins.
    
    * Fix test match setting.
    
    * Add end of line in prettierrc.json

---
+++ src/__tests__/dummy.ts
@@ -0,0 +1,713 @@
+const dummy: any = {
+  issueCount: 42,
+  pageInfo: {
+    hasNextPage: false,
+    endCursor: 'Y3Vyc29yOjQy',
+  },
+  nodes: [
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-06T09:38:11Z',
+      closed: true,
+      title: 'OW-543 fix toggle remove button',
+      additions: 14,
+      deletions: 5,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-06T08:49:07Z',
+      closed: true,
+      title: 'OP-459 apply style watch history',
+      additions: 46,
+      deletions: 97,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odx-player',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-06T08:44:21Z',
+      closed: true,
+      title: 'OP-455 add margin top for program at Tablet',
+      additions: 2,
+      deletions: 2,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odx-player',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-03T05:39:16Z',
+      closed: true,
+      title: 'Release/2019.5.1',
+      additions: 11,
+      deletions: 9,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-03T02:28:14Z',
+      closed: true,
+      title: 'OW-540 change copyright year in footer',
+      additions: 3,
+      deletions: 1,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-02T12:42:25Z',
+      closed: false,
+      title: 'OW-474 add program list redux state',
+      additions: 235,
+      deletions: 95,
+      state: 'OPEN',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-02T11:47:18Z',
+      closed: true,
+      title: 'Test code 작성 환경 추가.',
+      additions: 91,
+      deletions: 4,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'fe-assignment-lab',
+        owner: {
+          login: 'learn-programmers',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-02T08:14:15Z',
+      closed: true,
+      title: 'OW-539 [HOTFIX] fix disappear next episode and program information.',
+      additions: 8,
+      deletions: 8,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-02T07:37:38Z',
+      closed: true,
+      title: 'OW-539 fix disappear nextepisode',
+      additions: 719,
+      deletions: 753,
+      state: 'CLOSED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 8,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-02T04:31:02Z',
+      closed: true,
+      title: 'release/2020.1.0',
+      additions: 1,
+      deletions: 1,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odx-admin-front',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-02T01:54:12Z',
+      closed: true,
+      title: 'Change version to 2020.1.0',
+      additions: 2,
+      deletions: 2,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2020-01-01T09:26:13Z',
+      closed: true,
+      title: 'Add 2019 memoir',
+      additions: 176,
+      deletions: 12,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 4,
+      },
+      repository: {
+        name: 'lucas-wiki',
+        owner: {
+          login: 'stardustrain',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-31T07:03:59Z',
+      closed: true,
+      title: 'github actions test.',
+      additions: 6,
+      deletions: 6,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'lucas-wiki',
+        owner: {
+          login: 'stardustrain',
+        },
+      },
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-27T05:39:40Z',
+      closed: true,
+      title: 'OP-434 add pull down refresh in watch histroy screen',
+      additions: 21,
+      deletions: 3,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odx-player',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-27T02:33:20Z',
+      closed: true,
+      title: 'OP-433 add pull down refresh in favorite screen',
+      additions: 30,
+      deletions: 2,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odx-player',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-26T07:19:47Z',
+      closed: true,
+      title: 'Release/2019.5.0',
+      additions: 505,
+      deletions: 162,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 3,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-26T07:17:59Z',
+      closed: true,
+      title: 'Add optional item  and rules for codes.',
+      additions: 5,
+      deletions: 1,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odk-frontend-homework',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'Issue',
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-19T08:56:37Z',
+      closed: true,
+      title: 'Release/2019.4.0',
+      additions: 2694,
+      deletions: 1061,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 12,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-19T08:44:24Z',
+      closed: true,
+      title: 'OW-513 apply scroll fetch to user page',
+      additions: 141,
+      deletions: 115,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 5,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-19T05:36:49Z',
+      closed: true,
+      title: 'OP-403 apply watch history',
+      additions: 34,
+      deletions: 8,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odx-player',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-18T09:18:55Z',
+      closed: false,
+      title: 'OA-125 refactoring serializer',
+      additions: 658,
+      deletions: 49,
+      state: 'OPEN',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 4,
+      },
+      repository: {
+        name: 'odx-admin-front',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-18T07:53:31Z',
+      closed: true,
+      title: 'OA-124 change CCLanguage selector value.',
+      additions: 2,
+      deletions: 2,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 2,
+      },
+      repository: {
+        name: 'odx-admin-front',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-17T07:53:37Z',
+      closed: true,
+      title: 'OP-419 category landing 프로그램 목록에 on-air status 적용',
+      additions: 29,
+      deletions: 3,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odx-player',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-17T02:20:34Z',
+      closed: true,
+      title: 'OW-516 fix call program list api twice.',
+      additions: 1,
+      deletions: 0,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-16T09:38:34Z',
+      closed: true,
+      title: 'OW-472 add redux',
+      additions: 363,
+      deletions: 47,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 8,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-13T05:09:29Z',
+      closed: true,
+      title: 'OW-492 move language selector',
+      additions: 1,
+      deletions: 1,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-12T05:10:39Z',
+      closed: true,
+      title: 'OW-492 move language selector',
+      additions: 360,
+      deletions: 105,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 9,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-11T08:34:41Z',
+      closed: true,
+      title: 'OW-461 carousel arrow remove hover action.',
+      additions: 2,
+      deletions: 17,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 1,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-11T06:30:47Z',
+      closed: true,
+      title: 'Release/2019.3.0',
+      additions: 296,
+      deletions: 38,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 6,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-09T07:30:47Z',
+      closed: true,
+      title: 'OW-472 add redux',
+      additions: 384,
+      deletions: 40,
+      state: 'CLOSED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 3,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-06T05:38:23Z',
+      closed: true,
+      title: 'OW-510 episode list load bug',
+      additions: 3,
+      deletions: 3,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 3,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'PullRequest',
+      createdAt: '2019-12-03T10:05:06Z',
+      closed: true,
+      title: 'OW-460 change load more action',
+      additions: 143,
+      deletions: 36,
+      state: 'MERGED',
+      author: {
+        login: 'stardustrain',
+      },
+      commits: {
+        totalCount: 6,
+      },
+      repository: {
+        name: 'odc-frontend',
+        owner: {
+          login: 'ondemandkorea',
+        },
+      },
+    },
+    {
+      __typename: 'Issue',
+    },
+  ],
+}
+
+export default dummy
---
+++ src/__tests__/utils.spec.ts
@@ -0,0 +1,111 @@
+import {
+  getFilteredPullrequest,
+  getLineCount,
+  getProjectsGroupbyRepository,
+  getPersonalProjects,
+  getCommitsCount,
+  generatePRInformation,
+  getContributionByRepository,
+} from '../utils'
+import dummy from './dummy'
+
+describe('utils.ts', () => {
+  const filterdPullrequest = getFilteredPullrequest(dummy.nodes)
+
+  describe('getPullrequestCount(nodes: ResultNode[])', () => {
+    test('should return array of object only PullRequest type.', () => {
+      expect(filterdPullrequest.length).toEqual(32)
+    })
+  })
+
+  describe('getLineCount(prNodes: PullrequestNode[])', () => {
+    test('should retun total additions, deletions counts.', () => {
+      expect(getLineCount(dummy.nodes)).toEqual({
+        additions: 6986,
+        deletions: 2688,
+      })
+    })
+  })
+
+  describe('getProjectsGroupbyRepository(prNodes: PullrequestNode[])', () => {
+    test('should return groupped object by repository name.', () => {
+      const t = getProjectsGroupbyRepository(filterdPullrequest)
+
+      expect(t).toHaveProperty('odc-frontend')
+      expect(Object.keys(t).length).toEqual(6)
+      expect(t['lucas-wiki']).toEqual([
+        {
+          __typename: 'PullRequest',
+          additions: 176,
+          author: { login: 'stardustrain' },
+          closed: true,
+          commits: { totalCount: 4 },
+          createdAt: '2020-01-01T09:26:13Z',
+          deletions: 12,
+          repository: { name: 'lucas-wiki', owner: { login: 'stardustrain' } },
+          state: 'MERGED',
+          title: 'Add 2019 memoir',
+        },
+        {
+          __typename: 'PullRequest',
+          additions: 6,
+          author: { login: 'stardustrain' },
+          closed: true,
+          commits: { totalCount: 1 },
+          createdAt: '2019-12-31T07:03:59Z',
+          deletions: 6,
+          repository: { name: 'lucas-wiki', owner: { login: 'stardustrain' } },
+          state: 'MERGED',
+          title: 'github actions test.',
+        },
+      ])
+    })
+  })
+
+  describe('getPersonalProjects(prNodes: PullrequestNode[])', () => {
+    test('should return array of object that only personal project repository.', () => {
+      expect(getPersonalProjects(filterdPullrequest).length).toEqual(2)
+    })
+  })
+
+  describe('getCommitsCount(prNodes: PullrequestNode[])', () => {
+    test('should return all of commit count.', () => {
+      expect(getCommitsCount(filterdPullrequest)).toEqual(99)
+    })
+  })
+
+  describe('generatePRInformation(prNodes: PullrequestNode[])', () => {
+    test('should return information object from pull request nodes.', () => {
+      expect(generatePRInformation(filterdPullrequest)).toEqual({
+        lines: {
+          additions: 6986,
+          deletions: 2688,
+        },
+        commits: 99,
+        totalPRCount: 32,
+      })
+    })
+  })
+
+  describe('getContributionByRepository(projects: GroupedProject)', () => {
+    test('', () => {
+      expect(getContributionByRepository(getProjectsGroupbyRepository(filterdPullrequest))).toEqual([
+        ['odc-frontend', { commits: 75, lines: { additions: 5885, deletions: 2498 }, totalPRCount: 19 }],
+        ['odx-player', { commits: 10, lines: { additions: 162, deletions: 115 }, totalPRCount: 6 }],
+        ['odx-admin-front', { commits: 7, lines: { additions: 661, deletions: 52 }, totalPRCount: 3 }],
+        ['lucas-wiki', { commits: 5, lines: { additions: 182, deletions: 18 }, totalPRCount: 2 }],
+        ['fe-assignment-lab', { commits: 1, lines: { additions: 91, deletions: 4 }, totalPRCount: 1 }],
+        ['odk-frontend-homework', { commits: 1, lines: { additions: 5, deletions: 1 }, totalPRCount: 1 }],
+      ])
+
+      expect(getContributionByRepository(getProjectsGroupbyRepository(filterdPullrequest), 'commits')).toEqual([
+        ['odc-frontend', { commits: 75, lines: { additions: 5885, deletions: 2498 }, totalPRCount: 19 }],
+        ['odx-player', { commits: 10, lines: { additions: 162, deletions: 115 }, totalPRCount: 6 }],
+        ['odx-admin-front', { commits: 7, lines: { additions: 661, deletions: 52 }, totalPRCount: 3 }],
+        ['lucas-wiki', { commits: 5, lines: { additions: 182, deletions: 18 }, totalPRCount: 2 }],
+        ['fe-assignment-lab', { commits: 1, lines: { additions: 91, deletions: 4 }, totalPRCount: 1 }],
+        ['odk-frontend-homework', { commits: 1, lines: { additions: 5, deletions: 1 }, totalPRCount: 1 }],
+      ])
+    })
+  })
+})
