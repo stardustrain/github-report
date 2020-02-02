b29d5302238797635b65f451bfc5d0ecff458be4
Lucas Han <kthanterran@gmail.com>
2020-02-02 21:13:00 +0900

    resolve #3 setting for deploy. (#9)
    * resolve #3 setting for deploy.
    
    * Fix broken test.

--- src/__tests__/messages.spec.ts
+++ src/__tests__/messages.spec.ts
@@ -6,7 +6,7 @@ describe('messages.ts', () => {
   describe('generateContributionMessage(contributions: Contributions)', () => {
     test('should return generated contribution messages.', () => {
       expect(generateContributionMessage(contributionsDummy)).toEqual(
-        '*프로젝트기여 TOP 5*\n1. odc-frontend(59%): 19 pull requests / 75 commits.\n2. odx-player(19%): 6 pull requests / 10 commits.\n3. odx-admin-front(9%): 3 pull requests / 7 commits.\n4. lucas-wiki(6%): 2 pull requests / 5 commits.\n5. fe-assignment-lab(3%): 1 pull requests / 1 commits.'
+        '*프로젝트기여 TOP 5*\n1. odc-frontend (59%): *19* pull requests | 75 commits.\n2. odx-player (19%): *6* pull requests | 10 commits.\n3. odx-admin-front (9%): *3* pull requests | 7 commits.\n4. lucas-wiki (6%): *2* pull requests | 5 commits.\n5. fe-assignment-lab (3%): *1* pull requests | 1 commits.'
       )
     })
 
